24fe302adcdb3fcf2f8aa8aff9bfd730685603ba
cynthia.quach <cynthia.quach@ahmchealth.com>
2017-12-26 15:29:00 -0800

    JS Stalker exercise optional extra variable

--- JS/JS/stalkerScript.js
+++ JS/JS/stalkerScript.js
@@ -2,5 +2,7 @@ var firstName = prompt("What is your first name?");
 var lastName = prompt("What is your last name?");
 var age = prompt("How old are you?");
 
-console.log("Your full name is " + firstName + " " + lastName);
+var fullName = firstName + " " + lastName;
+
+console.log("Your full name is " + fullName);
 console.log("You are " + age + " years old");
