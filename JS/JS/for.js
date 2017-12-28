f1cf3c721b0b7402d2044a5f7f530c9706cd9b95
cynthia.quach <ccquach93@gmail.com>
2017-12-28 11:32:54 -0800

    JS For loops

---
+++ JS/JS/for.js
@@ -0,0 +1,33 @@
+console.log("PRINT ALL NUMBERS BETWEEN -10 AND 19");
+
+for(var i = -10; i < 20; i++) {
+	console.log(i);
+}
+
+console.log("PRINT ALL EVEN NUMBERS BETWEEN 10 AND 40");
+
+// for(var i = 10; i <= 40; i += 2) {
+// 	console.log(i);
+// }
+
+for(var i = 10; i <= 40; i++) {
+	if(i % 2 === 0) {
+		console.log(i);
+	}
+}
+
+console.log("PRINT ALL ODD NUMBERS BETWEEN 300 AND 333");
+
+for(var i = 300; i <= 333; i++) {
+	if(i % 2 !== 0) {
+		console.log(i);
+	}
+}
+
+console.log("PRINT ALL NUMBERS DIVISIBLE BY 5 AND 3 BETWEEN 5 AND 50");
+
+for(var i = 5; i <= 50; i++) {
+	if(i % 5 === 0 && i % 3 === 0) {
+		console.log(i);
+	}
+}
