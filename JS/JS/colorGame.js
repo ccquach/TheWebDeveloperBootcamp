e8df7c68b3cd13c384bcad70c7067b48a93f50e5
cynthia.quach <ccquach93@gmail.com>
2017-12-30 18:43:41 -0800

    JS Color Game: add Easy & Hard mode buttons; add functionality to Easy button

--- JS/JS/colorGame.js
+++ JS/JS/colorGame.js
@@ -5,6 +5,24 @@ var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function() {
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	colors = generateRandomColors(3);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++) {
+		colors[i] ? squares[i].style.background = colors[i]: squares[i].style.display = "none";
+	}
+});
+
+hardBtn.addEventListener("click", function() {
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+});
 
 resetButton.addEventListener("click", function() {
 	//generate all new colors
