3cc493224f717fc47185f1a0e1ad62e68ab59107
cynthia.quach <ccquach93@gmail.com>
2017-12-30 17:44:03 -0800

    JS Color Game: add click listeners to squares and compare clicked color to picked color

--- JS/JS/colorGame.js
+++ JS/JS/colorGame.js
@@ -14,6 +14,18 @@ var colorDisplay = document.querySelector("#colorDisplay");
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
+	//add initial colors to squares
 	squares[i].style.backgroundColor = colors[i];
-}
 
+	//add click listeners to squares
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked square
+		var clickedColor = this.style.backgroundColor;
+		//compare clicked color with pickedColor
+		if(clickedColor === pickedColor) {
+			alert("correct");
+		} else {
+			alert("wrong");
+		}
+	});
+}
