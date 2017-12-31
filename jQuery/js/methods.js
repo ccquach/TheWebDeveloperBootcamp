6bc187ee0143b516da21602978b7d5a74f1fa86e
cynthia.quach <ccquach93@gmail.com>
2017-12-31 11:56:05 -0800

    jQuery manipulating text, HTML, attributes, values

---
+++ jQuery/js/methods.js
@@ -0,0 +1,29 @@
+// ********** .text() **********
+
+// $('hi').text();
+// $('ul').text();
+// $('li').text();
+// $('h1').text("New Text!!!");
+// $('li').text("Rusty, Colt's dog, is adorable");
+
+// ********** .html() **********
+
+// $('ul').html();
+// $('ul').html("<li>I hacked your UL!</li><li>Rusty is still adorable!</li>");
+// $('li').html("<a href='http://www.google.com'>CLICK ME TO GO TO GOOGLE</a>");
+
+// ********** .attr() **********
+
+$('img').css("width", "200px");
+// $('img').attr("src", "https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg");
+$('img:first-of-type').attr("src", "https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg");
+$('img').last().attr("src", "https://c3.staticflickr.com/3/2418/2243463214_f32ab004af_b.jpg");
+$('img').attr("src", "https://c1.staticflickr.com/2/1445/23931141974_a71b3f5483_b.jpg");
+// $('input').attr("type", "color");
+
+// ********** .val() **********
+
+// $('input').val();
+$('input').val("Rusty Steele");
+$('input').val("");
+$('select').val();
