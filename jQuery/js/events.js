cb8db1f0789be6a83505d401ceadfb65de29707c
cynthia.quach <ccquach93@gmail.com>
2017-12-31 15:08:22 -0800

    jQuery on event

--- jQuery/js/events.js
+++ jQuery/js/events.js
@@ -1,17 +1,33 @@
-$('h1').click(function() {
-	alert("h1 clicked");
+// $('h1').click(function() {
+// 	alert("h1 clicked");
+// });
+
+// $('button').click(function() {
+// 	// alert("button clicked");
+// 	// $(this).css("background", "pink");
+// 	var text = $(this).text();
+// 	console.log("you clicked " + text);
+// });
+
+// $('input[type="text"]').keypress(function(event) {
+// 	// console.log(event);
+// 	if(event.which === 13) {
+// 		alert("you hit enter");
+// 	}
+// });
+
+$('h1').on('click', function() {
+	$(this).css('color', 'purple');
 });
 
-$('button').click(function() {
-	// alert("button clicked");
-	// $(this).css("background", "pink");
-	var text = $(this).text();
-	console.log("you clicked " + text);
+$("input[type='text']").on("keypress", function() {
+	console.log("key pressed");
 });
 
-$('input[type="text"]').keypress(function(event) {
-	// console.log(event);
-	if(event.which === 13) {
-		alert("you hit enter");
-	}
+$("button").on("mouseenter", function() {
+	$(this).css("font-weight", "bold");
+});
+
+$("button").on("mouseleave", function() {
+	$(this).css("font-weight", "normal");
 });
