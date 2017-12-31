779099505485945938c33bcfa22a39bc73b248e4
cynthia.quach <ccquach93@gmail.com>
2017-12-31 11:26:34 -0800

    jQuery selectors

---
+++ jQuery/js/selector.js
@@ -0,0 +1,11 @@
+//select all divs and give them a purple background
+$("div").css("background", "purple");
+
+//select the divs with class "highlight" and make them 200px wide
+$("div.highlight").css("width", "200px");
+
+//select the div with id "third" and give it an orange border
+$("#third").css("border", "2px solid orange");
+
+//Bonus: select the first div only and change its font color to pink
+$("div:first-of-type").css("color", "pink");
