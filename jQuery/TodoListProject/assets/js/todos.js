fb1c4f9c712499ce65e7d42cbc2b439fc46c8e05
cynthia.quach <ccquach93@gmail.com>
2017-12-31 16:06:23 -0800

    Todo List Project delete todos

--- jQuery/TodoListProject/assets/js/todos.js
+++ jQuery/TodoListProject/assets/js/todos.js
@@ -2,3 +2,11 @@
 $("li").on("click", function() {
 	$(this).toggleClass("completed");
 });
+
+//click on X to delete todo
+$("span").on("click", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
