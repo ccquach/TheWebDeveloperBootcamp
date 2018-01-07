25849f865bb2e460a68f354bdccffaec5130e417
cynthia.quach <ccquach93@gmail.com>
2018-01-07 08:01:39 -0800

    EJS conditionals and loops: syntax to render to html vs to evaluate code

--- Node/EJSDemo/app.js
+++ Node/EJSDemo/app.js
@@ -12,6 +12,15 @@ app.get("/fallinlovewith/:thing", function(req, res) {
 	// res.send("you fell in love with " + thing);
 });
 
+app.get("/posts", function(req, res) {
+	var posts = [
+		{ title: "Post 1", author: "Susy" },
+		{ title: "My adorable pet bunny", author: "Charlie" },
+		{ title: "Can you believe this pomsky?", author: "Colt" }
+	];
+	res.render("posts.ejs", { posts: posts });
+});
+
 app.listen(3000, function() {
 	console.log("Serving EJS Demo app on Port 3000");
 });
--- Node/objectives.md
+++ Node/objectives.md
@@ -88,3 +88,9 @@
 * Use res.render() to render HTML (from an EJS file)
 * Explain what EJS (Embedded JavaScript) is and why we use it
 * Pass variables to EJS templates
+
+#EJS Control Flow
+
+* Show examples of control flow in EJS templates
+* Write if statements in an EJS file
+* Write loops in an EJS file
