c4d6b569e6a6e43ba9e80310ab2372d7ff2b763f
cynthia.quach <ccquach93@gmail.com>
2018-01-07 08:27:10 -0800

    Express app including stylesheets and partial views

--- Node/EJSDemo/app.js
+++ Node/EJSDemo/app.js
@@ -1,14 +1,20 @@
 var express = require("express");
 var app = express();
 
+// serve the contents of the 'public' directory
+app.use(express.static("public"));
+// express will expect ejs files in res.render();
+// avoid having to explicitly use ejs extension when passing parameter
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-	res.render("home.ejs");
+	res.render("home");
 	// res.send("<h1>Welcome to the home page!<h1><h2>blah blah</h2>");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res) {
 	var thing = req.params.thing;
-	res.render("love.ejs", { thingVar: thing });
+	res.render("love", { thingVar: thing });
 	// res.send("you fell in love with " + thing);
 });
 
@@ -18,7 +24,7 @@ app.get("/posts", function(req, res) {
 		{ title: "My adorable pet bunny", author: "Charlie" },
 		{ title: "Can you believe this pomsky?", author: "Colt" }
 	];
-	res.render("posts.ejs", { posts: posts });
+	res.render("posts", { posts: posts });
 });
 
 app.listen(3000, function() {
--- Node/objectives.md
+++ Node/objectives.md
@@ -94,3 +94,9 @@
 * Show examples of control flow in EJS templates
 * Write if statements in an EJS file
 * Write loops in an EJS file
+
+#Styles and Partials
+
+* Show how to properly include public assets
+* Properly configure our app to use EJS
+* Use partials to dry up our code!
