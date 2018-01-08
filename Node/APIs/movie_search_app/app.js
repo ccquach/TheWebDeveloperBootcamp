c96c822c4089dceaf9b9160b0caab7ec15955a10
cynthia.quach <ccquach93@gmail.com>
2018-01-07 17:31:24 -0800

    Movie Search App displaying data

--- Node/APIs/movie_search_app/app.js
+++ Node/APIs/movie_search_app/app.js
@@ -7,8 +7,8 @@ app.set("view engine", "ejs");
 app.get("/results", function(req, res) {
 	request("http://omdbapi.com/?s=star&apikey=thewdb", function(error, response, body) {
 		if(!error && response.statusCode == 200) {
-			var results = JSON.parse(body);
-			res.send(results["Search"][0].Title);
+			var data = JSON.parse(body);
+			res.render("results", { data: data });
 		}
 	});
 });
