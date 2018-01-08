e7f60da18ed22b38409af36cfe5d5fbf0a7382d2
cynthia.quach <ccquach93@gmail.com>
2018-01-07 17:51:32 -0800

    Movie Search App added search

--- Node/APIs/movie_search_app/app.js
+++ Node/APIs/movie_search_app/app.js
@@ -4,15 +4,21 @@ var request = require("request");
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+	res.render("search");
+});
+
 app.get("/results", function(req, res) {
-	request("http://omdbapi.com/?s=star&apikey=thewdb", function(error, response, body) {
+	var query = req.query.search;
+	var url = "http://omdbapi.com/?s=" + query + "&apikey=thewdb";
+
+	request(url, function(error, response, body) {
 		if(!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 			res.render("results", { data: data });
 		}
 	});
 });
-
 
 app.listen(3000, function() {
 	console.log("Serving Movie Search Application on port 3000");
