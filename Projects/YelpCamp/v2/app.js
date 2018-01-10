4ea373382d1ddf630cc9ad6e8bb42577fa240c29
cynthia.quach <ccquach93@gmail.com>
2018-01-09 21:37:25 -0800

    Added description field to campground model; added show route

--- Projects/YelpCamp/v2/app.js
+++ Projects/YelpCamp/v2/app.js
@@ -10,28 +10,31 @@ app.set("view engine", "ejs");
 //schema setup
 var campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-// Campground.create(
-// {
-// 	name: "Granite Hill",
-// 	image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"
+Campground.create(
+{
+	name: "Granite Hill",
+	image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg",
+	description: "This is a huge granite hill, no bathrooms. No water. Beautifull granite!"
 
-// }, function(err, campground) {
-// 	if(err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log("NEWLY CREATED CAMPGROUND: ");
-// 		console.log(campground);
-// 	}
-// });
+}, function(err, campground) {
+	if(err) {
+		console.log(err);
+	} else {
+		console.log("NEWLY CREATED CAMPGROUND: ");
+		console.log(campground);
+	}
+});
 
 app.get("/", function(req, res) {
 	res.render("landing");
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res) {
 	//get all campgrounds from db
 	Campground.find({}, function(err, allCampgrounds) {
@@ -43,6 +46,7 @@ app.get("/campgrounds", function(req, res) {
 	});
 });
 
+//CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res) {
 	//get data from form and add to campgrounds collection
 	var name = req.body.name;
@@ -62,8 +66,15 @@ app.post("/campgrounds", function(req, res) {
 	});
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
 	res.render("new");
+});
+
+app.get("/campgrounds/:id", function(req, res) {
+	//find the campground with provided ID
+	//render show template with that campground
+	res.send("this will be the show page one day");
 });
 
 app.listen(3000, function() {
--- Projects/YelpCamp/v2/readme.md
+++ Projects/YelpCamp/v2/readme.md
@@ -28,3 +28,18 @@ Each Campground has:
 * Install and configure mongoose
 * Setup campground model
 * Use campground model inside of our routes
+
+#Show Page
+* Review the RESTful routes we've seen so far
+* Add description to our campground model
+* Show db.collection.drop()
+* Add a show route/template
+
+RESTful ROUTES
+
+name 	  url		verb 	description
+==========================================================
+INDEX 	/dogs		GET		Display a list of all dogs
+NEW 	/dogs/new  	GET		Display form to make a new dog
+CREATE  /dogs		POST	Add new dog to DB
+SHOW    /dogs/:id   GET		Show info about one dog
