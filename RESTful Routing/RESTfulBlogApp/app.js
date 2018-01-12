e4fbb733848f618f74164f165c22ed5e79398127
cynthia.quach <ccquach93@gmail.com>
2018-01-11 21:42:05 -0800

    RESTful Blog App NEW and CREATE routes

--- RESTful Routing/RESTfulBlogApp/app.js
+++ RESTful Routing/RESTfulBlogApp/app.js
@@ -23,6 +23,8 @@ app.get("/", function(req, res) {
 	res.redirect("/blogs");
 });
 
+
+// INDEX ROUTE
 app.get("/blogs", function(req, res) {
 	Blog.find({}, function(err, blogs) {
 		if(err) {
@@ -33,6 +35,23 @@ app.get("/blogs", function(req, res) {
 	})
 });
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+	res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res) {
+	// create blog
+	Blog.create(req.body.blog, function(err, newBlog) {
+		if(err) {
+			res.render("new");
+		} else {
+			// then, redirect to index
+			res.redirect("/blogs");
+		}
+	});
+});
 
 app.listen(3000, function() {
 	console.log("Serving RESTful Blog App on port 3000")
--- RESTful Routing/readme.md
+++ RESTful Routing/readme.md
@@ -26,3 +26,9 @@ DELETE	/destroyBlog/:id
 * Add Header and Footer Partials
 * Include Semantic UI
 * Add Simple Nav
+
+#Putting the C in CRUD
+* Add NEW route
+* Add NEW template
+* Add CREATE route
+* Add CREATE template
