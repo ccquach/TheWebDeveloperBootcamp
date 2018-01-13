4828e1b201d096950662815cb4f1da2043b2e029
cynthia.quach <ccquach93@gmail.com>
2018-01-12 20:41:51 -0800

    RESTful Blog App DELETE route

--- RESTful Routing/RESTfulBlogApp/app.js
+++ RESTful Routing/RESTfulBlogApp/app.js
@@ -88,6 +88,17 @@ app.put("/blogs/:id", function(req, res) {
 	});
 });
 
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res) {
+	Blog.findByIdAndRemove(req.params.id, function(err) {
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
+		}
+	});
+});
+
 app.listen(3000, function() {
 	console.log("Serving RESTful Blog App on port 3000")
 });
