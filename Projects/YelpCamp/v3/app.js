83d01c8379ae05c95eabaefb70a5d21292b079f2
cynthia.quach <ccquach93@gmail.com>
2018-01-15 14:30:48 -0800

    YelpCamp App populate comments in campgrounds show route and display on show page

--- Projects/YelpCamp/v3/app.js
+++ Projects/YelpCamp/v3/app.js
@@ -5,25 +5,11 @@ var express 	= require("express"),
 	Campground 	= require("./models/campground"),
 	seedDB		= require("./seeds");
 
-seedDB();
 mongoose.connect("mongodb://127.0.0.1/yelp_camp_v3", { useMongoClient: true });
+mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
-// Campground.create(
-// {
-// 	name: "Granite Hill",
-// 	image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg",
-// 	description: "This is a huge granite hill, no bathrooms. No water. Beautifull granite!"
-
-// }, function(err, campground) {
-// 	if(err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log("NEWLY CREATED CAMPGROUND: ");
-// 		console.log(campground);
-// 	}
-// });
+seedDB();
 
 app.get("/", function(req, res) {
 	res.render("landing");
@@ -71,7 +57,7 @@ app.get("/campgrounds/new", function(req, res) {
 //SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res) {
 	//find the campground with provided ID
-	Campground.findById(req.params.id, function(err, foundCampground) {
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
 		if(err) {
 			console.log(err);
 		} else {
