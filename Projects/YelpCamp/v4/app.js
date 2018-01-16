03c6b9d790fdcc625d2540778e5cfbdd2cbf1358
cynthia.quach <ccquach93@gmail.com>
2018-01-15 20:32:39 -0800

    YelpCamp App create comments post route and add button to show page taking user to new comment form

--- Projects/YelpCamp/v4/app.js
+++ Projects/YelpCamp/v4/app.js
@@ -3,6 +3,7 @@ var express 	= require("express"),
 	bodyParser 	= require("body-parser"),
 	mongoose 	= require("mongoose"),
 	Campground 	= require("./models/campground"),
+	Comment 	= require("./models/comment"),
 	seedDB		= require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp_v4");
@@ -78,6 +79,29 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
 			res.render("comments/new", { campground: campground });
 		}
 	})
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+	// Lookup campground using Id
+	Campground.findById(req.params.id, function(err, campground) {
+		if(err) {
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			// Create new comment
+			Comment.create(req.body.comment, function(err, comment) {
+				if(err) {
+					console.log(err);
+				} else {
+					// Connect new comment to campground
+					campground.comments.push(comment._id);
+					campground.save();
+					// Redirect to campground show page
+					res.redirect("/campgrounds/" + req.params.id);
+				}
+			});
+		}
+	});
 });
 
 app.listen(3000, function() {
