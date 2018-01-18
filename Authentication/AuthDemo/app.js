50095e7c15897aae6708abb707d659e36c8cbf70
cynthia.quach <ccquach93@gmail.com>
2018-01-17 21:25:47 -0800

    Auth demo app add register route/form

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -6,12 +6,17 @@ var express					= require("express"),
 	localStrategy 			= require("passport-local"),
 	passportLocalMongoose	= require("passport-local-mongoose");
 
+// ==========================
 // APP CONFIG
+// ==========================
 mongoose.connect("mongodb://localhost/auth_demo_app");
 var app	= express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// ==========================
 // PASSPORT CONFIG
+// ==========================
 app.use(require("express-session")({
 	secret: "Rusty is the best and cutest dog in the world",
 	resave: false,
@@ -23,13 +28,34 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// ==========================
 // ROUTES
+// ==========================
 app.get("/", function(req, res) {
 	res.render("home");
 });
 
 app.get("/secret", function(req, res) {
 	res.render("secret");
+});
+
+// AUTH ROUTES
+// Show sign up form
+app.get("/register", function(req, res) {
+	res.render("register");
+});
+
+// Handling user sign up
+app.post("/register", function(req, res) {
+	User.register(new User({ username: req.body.username }), req.body.password, function(err, user) {
+		if(err) {
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function() {
+			res.redirect("secret");
+		});
+	});
 });
 
 app.listen(3000, function() {
