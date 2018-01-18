2fa44cb00320992b155ba1da7332856f4349af3b
cynthia.quach <ccquach93@gmail.com>
2018-01-17 20:53:42 -0800

    Authentication demo intro

---
+++ Authentication/AuthDemo/app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/auth_demo_app");
+
+var app	= express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+	res.render("home");
+});
+
+app.get("/secret", function(req, res) {
+	res.render("secret");
+});
+
+app.listen(3000, function() {
+	console.log("Serving Auth Demo App on port 3000");
+});
---
+++ Authentication/readme.md
@@ -0,0 +1,17 @@
+#Authentication
+
+#Intro to Auth
+* What tools are we using?
+	* Passport
+	* Passport Local
+	* Passport Local Mongoose
+* Walk through auth flow
+* Discuss sessions
+	* Express-Session
+	
+
+#Auth Code Along Part 1
+* Set up folder structure
+* Install needed packages
+* Add root route and template
+* Add secret route and template
