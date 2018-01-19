7b021aedecbe1396b655cc72d0c939b8a4a35f45
cynthia.quach <ccquach93@gmail.com>
2018-01-19 14:06:38 -0800

    YelpCamp App associate users and comments; prevent user from creating new campground unless logged in

--- Projects/YelpCamp/readme.md
+++ Projects/YelpCamp/readme.md
@@ -94,3 +94,14 @@ CREATE 	/campgrounds/:id/comments		POST
 
 #Auth Pt. 5 - Show/Hide Links
 * Show/hide auth links correctly
+
+#Refactor the Routes
+* Use Express router to reorganize all routes
+
+#Users + Comments
+* Associate users and comments
+* Save author's name to a comment automatically
+
+#Users + Campgrounds
+* Prevent an unauthenticated user from creating a campground
+* Save username+id to newly created campground
---
+++ Projects/YelpCamp/v8/routes/campgrounds.js
@@ -0,0 +1,65 @@
+var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campground");
+
+//INDEX - show all campgrounds
+router.get("/", function(req, res) {
+	//get all campgrounds from db
+	Campground.find({}, function(err, allCampgrounds) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds/index", { campgrounds: allCampgrounds });
+		}
+	});
+});
+
+//CREATE - add new campground to DB
+router.post("/", isLoggedin, function(req, res) {
+	//get data from form and add to campgrounds collection
+	var name 	= req.body.name,
+		image 	= req.body.image,
+		desc 	= req.body.description
+	var newCampgound = {
+		name: name,
+		image: image,
+		description: desc
+	}
+	//create a new campground and save to db
+	Campground.create(newCampgound, function(err, newlyCreated) {
+		if(err) {
+			console.log(err);
+		} else {
+			//redirect back to campgrounds page
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+//NEW - show form to create new campground
+router.get("/new", isLoggedin, function(req, res) {
+	res.render("campgrounds/new");
+});
+
+//SHOW - shows more info about one campground
+router.get("/:id", function(req, res) {
+	//find the campground with provided ID
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+		if(err) {
+			console.log(err);
+		} else {
+			//render show template with that campground
+			res.render("campgrounds/show", { campground: foundCampground });
+		}
+	});
+});
+
+//middleware
+function isLoggedin(req, res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
+
+module.exports = router
