2f137ad1c79a2eca426e5b28e5097616e433a27d
cynthia.quach <ccquach93@gmail.com>
2018-01-20 09:36:43 -0800

    YelpCamp App new features

---
+++ Projects/YelpCamp/v9/routes/comments.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var router = express.Router({ mergeParams: true });
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+// Comments new
+router.get("/new", isLoggedin, function(req, res) {
+	Campground.findById(req.params.id, function(err, campground) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("comments/new", { campground: campground });
+		}
+	})
+});
+
+// Comments create
+router.post("/", isLoggedin, function(req, res) {
+	// Lookup campground using Id
+	Campground.findById(req.params.id, function(err, campground) {
+		if(err) {
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			// Create new comment
+			Comment.create(req.body.comment, function(err, comment) {
+				if(err) {
+					console.log(err);
+				} else {
+					// Add username and id to comment
+					comment.author.id = req.user._id;
+					comment.author.username = req.user.username;
+					// Save comment
+					comment.save();
+					// Connect new comment to campground
+					campground.comments.push(comment._id);
+					campground.save();
+					console.log(comment);
+					// Redirect to campground show page
+					res.redirect("/campgrounds/" + req.params.id);
+				}
+			});
+		}
+	});
+});
+
+//middleware
+function isLoggedin(req, res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
+
+module.exports = router;
