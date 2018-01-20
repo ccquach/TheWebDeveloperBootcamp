dab6fc46c86d6c4594c62c1813154546636624ca
cynthia.quach <ccquach93@gmail.com>
2018-01-20 11:05:28 -0800

    Add authorization to edit/delete campgrounds and to see edit/delete buttons

--- Projects/YelpCamp/v9/routes/campgrounds.js
+++ Projects/YelpCamp/v9/routes/campgrounds.js
@@ -61,18 +61,14 @@ router.get("/:id", function(req, res) {
 });
 
 //EDIT - show form to edit campground
-router.get("/:id/edit", function(req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
 	Campground.findById(req.params.id, function(err, foundCampground) {
-		if(err) {
-			res.redirect("/campgrounds");
-		} else {
-			res.render("campgrounds/edit", { campground: foundCampground });
-		}
+		res.render("campgrounds/edit", { campground: foundCampground });
 	});
 });
 
 //UPDATE - apply edits to DB
-router.put("/:id", function(req, res) {
+router.put("/:id", checkCampgroundOwnership, function(req, res) {
 	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
 		if(err) {
 			res.redirect("/campgrounds");
@@ -83,7 +79,7 @@ router.put("/:id", function(req, res) {
 });
 
 //DESTROY - delete campground
-router.delete("/:id", function(req, res) {
+router.delete("/:id", checkCampgroundOwnership, function(req, res) {
 	Campground.findByIdAndRemove(req.params.id, function(err) {
 		if(err) {
 			res.redirect("/campgrounds");
@@ -99,6 +95,27 @@ function isLoggedin(req, res, next) {
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next) {
+	if(req.isAuthenticated()) {
+		Campground.findById(req.params.id, function(err, foundCampground) {
+			if(err) {
+				res.redirect("back");
+			} else {
+				// does user own the campground?
+				if(foundCampground.author.id.equals(req.user._id)) {
+					next();
+				// otherwise, redirect
+				} else {
+					res.redirect("back");
+				}
+			}
+		});
+	// if not, redirect
+	} else {
+		res.redirect("back");
+	}
 }
 
 module.exports = router
