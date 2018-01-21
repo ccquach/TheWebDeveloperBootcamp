0ace277332773b696ba9fc803fbaa564dbf42908
cynthia.quach <ccquach93@gmail.com>
2018-01-21 10:27:21 -0800

    Add bootstrap alerts

--- Projects/YelpCamp/v10/routes/index.js
+++ Projects/YelpCamp/v10/routes/index.js
@@ -46,6 +46,7 @@ router.post("/login", passport.authenticate("local",
 // logout route
 router.get("/logout", function(req, res) {
 	req.logout();
+	req.flash("success", "Logged you out!");
 	res.redirect("/campgrounds");
 });
 
