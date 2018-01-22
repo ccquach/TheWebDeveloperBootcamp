45e4f276312f9ff0a088e0d6ecf9ace046318c77
cynthia.quach <ccquach93@gmail.com>
2018-01-22 10:54:17 -0800

    Add edit/update and delete comment routes that require admin privileges

--- Projects/WorklistApp/routes/comments.js
+++ Projects/WorklistApp/routes/comments.js
@@ -46,4 +46,47 @@ router.post("/", middleware.isLoggedIn, function(req, res) {
 	});
 });
 
+// Comment edit
+router.get("/:comment_id/edit", middleware.isAdmin, function(req, res) {
+	Account.findById(req.params.id, function(err, foundAccount) {
+		if(err | !foundAccount) {
+			req.flash("error", "Account not found.");
+			return res.back();
+		}
+		Comment.findById(req.params.comment_id, function(err, foundComment) {
+			if(err || !foundComment) {
+				req.flash("error", "Comment not found.");
+				return res.back();
+			}
+			res.render("comments/edit", { account_id: req.params.id, comment: foundComment });
+		});
+	});
+});
+
+// Comment update
+router.put("/:comment_id", middleware.isAdmin, function(req, res) {
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+		if(err) {
+			req.flash("error", "Failed to update comment.");
+			res.back();
+		} else {
+			req.flash("success", "Successfully updated comment.");
+			res.redirect("/accounts/" + req.params.id);
+		}
+	});
+});
+
+// Comment destroy
+router.delete("/:comment_id", middleware.isAdmin, function(req, res) {
+	Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+		if(err) {
+			req.flash("error", "Failed to delete comment.");
+			res.back();
+		} else {
+			req.flash("success", "Successfully deleted comment.");
+			res.redirect("/accounts/" + req.params.id);
+		}
+	});
+});
+
 module.exports = router;
