abd9620cfb83ccdf0f6c6c84d5a6198abbeb64c7
cynthia.quach <ccquach93@gmail.com>
2018-01-22 09:30:57 -0800

    Update seed function

--- Projects/YelpCamp/v4/seeds.js
+++ Projects/YelpCamp/v4/seeds.js
@@ -6,17 +6,17 @@ var data = [
 	{
 		name: "Salmon Creek",
 		image: "https://farm4.staticflickr.com/3053/2586934044_339a678e73.jpg",
-		description: "Lots of salmon here"
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est tortor, tempor vitae suscipit vitae, consectetur sed lorem. Donec nisl dui, commodo ut lacinia nec, egestas nec arcu. Vestibulum tristique quis ligula semper tincidunt. Phasellus sodales pulvinar odio, quis commodo quam laoreet vel. Fusce sed lacus eget dui molestie suscipit ut id tortor. In gravida metus nec erat lacinia eleifend. Phasellus sagittis eleifend augue vitae interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac lectus nec orci ornare aliquet."
 	},
 	{
 		name: "Granite Hill",
 		image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg",
-		description: "Lots of granite here"
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est tortor, tempor vitae suscipit vitae, consectetur sed lorem. Donec nisl dui, commodo ut lacinia nec, egestas nec arcu. Vestibulum tristique quis ligula semper tincidunt. Phasellus sodales pulvinar odio, quis commodo quam laoreet vel. Fusce sed lacus eget dui molestie suscipit ut id tortor. In gravida metus nec erat lacinia eleifend. Phasellus sagittis eleifend augue vitae interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac lectus nec orci ornare aliquet."
 	},
 	{
 		name: "Mountain Goat's Rest",
 		image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg",
-		description: "Lots of goats here"
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec est tortor, tempor vitae suscipit vitae, consectetur sed lorem. Donec nisl dui, commodo ut lacinia nec, egestas nec arcu. Vestibulum tristique quis ligula semper tincidunt. Phasellus sodales pulvinar odio, quis commodo quam laoreet vel. Fusce sed lacus eget dui molestie suscipit ut id tortor. In gravida metus nec erat lacinia eleifend. Phasellus sagittis eleifend augue vitae interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac lectus nec orci ornare aliquet."
 	}
 ];
 
@@ -27,30 +27,36 @@ function seedDB() {
 			console.log(err);
 		}
 		console.log("removed campgrounds");
-		// Add a few campgrounds
-		data.forEach(function(seed) {
-			Campground.create(seed, function(err, campground) {
-				if(err) {
-					console.log(err);
-				} else {
-					console.log("added a campground");
-					// Create a comment
-					Comment.create(
-						{ 
-							text: "This place is great, but I wish there was internet.",
-							author: "Homer"
-						}, function(err, comment) {
-							if(err) {
-								console.log(err);
-							} else {
-								campground.comments.push(comment._id);
-								campground.save();
-								console.log("created new comment");
+		Comment.remove({}, function(err) {
+			if(err) {
+				console.log(err);
+			}
+			console.log("removed comments");
+			// Add a few campgrounds
+			data.forEach(function(seed) {
+				Campground.create(seed, function(err, campground) {
+					if(err) {
+						console.log(err);
+					} else {
+						console.log("added a campground");
+						// Create a comment
+						Comment.create(
+							{ 
+								text: "This place is great, but I wish there was internet.",
+								author: "Homer"
+							}, function(err, comment) {
+								if(err) {
+									console.log(err);
+								} else {
+									campground.comments.push(comment._id);
+									campground.save();
+									console.log("created new comment");
+								}
 							}
-						}
-					);
-				}
-			})
+						);
+					}
+				})
+			});
 		});
 	});
 }
--- Projects/YelpCamp/v8/seeds.js
+++ Projects/YelpCamp/v8/seeds.js
@@ -27,30 +27,36 @@ function seedDB() {
 			console.log(err);
 		}
 		console.log("removed campgrounds");
-		// Add a few campgrounds
-		data.forEach(function(seed) {
-			Campground.create(seed, function(err, campground) {
-				if(err) {
-					console.log(err);
-				} else {
-					console.log("added a campground");
-					// Create a comment
-					Comment.create(
-						{ 
-							text: "This place is great, but I wish there was internet.",
-							author: "Homer"
-						}, function(err, comment) {
-							if(err) {
-								console.log(err);
-							} else {
-								campground.comments.push(comment._id);
-								campground.save();
-								console.log("created new comment");
+		Comment.remove({}, function(err) {
+			if(err) {
+				console.log(err);
+			}
+			console.log("removed comments");
+			// Add a few campgrounds
+			data.forEach(function(seed) {
+				Campground.create(seed, function(err, campground) {
+					if(err) {
+						console.log(err);
+					} else {
+						console.log("added a campground");
+						// Create a comment
+						Comment.create(
+							{ 
+								text: "This place is great, but I wish there was internet.",
+								author: "Homer"
+							}, function(err, comment) {
+								if(err) {
+									console.log(err);
+								} else {
+									campground.comments.push(comment._id);
+									campground.save();
+									console.log("created new comment");
+								}
 							}
-						}
-					);
-				}
-			})
+						);
+					}
+				})
+			});
 		});
 	});
 }
