5dee1c6bf75d147e31fec6f0d9ba7f77e9060dce
cynthia.quach <ccquach93@gmail.com>
2018-01-24 13:31:45 -0800

    Style login/register views; update nav with dropdown menu for smaller screens

--- Projects/YelpCamp/v11/routes/index.js
+++ Projects/YelpCamp/v11/routes/index.js
@@ -13,7 +13,7 @@ router.get("/", function(req, res) {
 // ===========================================
 // show register form
 router.get("/register", function(req, res) {
-	res.render("register");
+	res.render("register", { page: "register" });
 });
 
 // handle sign up logic
@@ -25,7 +25,7 @@ router.post("/register", function(req, res) {
 			return res.redirect("/register");
 		}
 		passport.authenticate("local")(req, res, function() {
-			req.flash("success", "Welcome to YelpCamp, " + user.username);
+			req.flash("success", "Welcome to YelpCamp, " + user.username) + "!";
 			res.redirect("/campgrounds");
 		});
 	});
@@ -33,7 +33,7 @@ router.post("/register", function(req, res) {
 
 // show login form
 router.get("/login", function(req, res) {
-	res.render("login");
+	res.render("login", { page: "login" });
 });
 
 // handling login logic
