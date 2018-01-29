f150d6a24b4c4aa9ff0ef0262507436068aee1d3
cynthia.quach <ccquach93@gmail.com>
2018-01-29 11:20:54 -0800

    add createdAt to campground and comment model schemas

--- Projects/YelpCamp/v11/models/campground.js
+++ Projects/YelpCamp/v11/models/campground.js
@@ -4,6 +4,7 @@ var campgroundSchema = new mongoose.Schema({
 	name: String,
 	image: String,
 	description: String,
+	createdAt: { type: Date, default: Date.now },
 	author: {
 		id: {
 			type: mongoose.Schema.Types.ObjectId,
